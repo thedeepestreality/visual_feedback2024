70ebbe08758607f3f21b80b5ca9776b15a5cfdb9
thedeepestreality <zetoster@gmail.com>
2024-03-22 03:36:37 +0300

    working cartesian space control

--- pybullet/pendulum.py
+++ pybullet/pendulum.py
@@ -14,7 +14,11 @@ import numpy as np
 dt = 1/240 # pybullet simulation step
 q0 = 0.5  # starting position (radian)
 qd = 0.5
-L = 0.8
+
+xd = 0.1
+zd = 1.0
+
+L = 0.5
 Z0 = 1.5
 pos = q0
 maxTime = 5
@@ -27,6 +31,7 @@ logVel = np.zeros(sz)
 
 jointIndices = [1,3]
 eefLinkIdx = 4
+eefLinkIdx = 2
 
 physicsClient = p.connect(p.GUI) # or p.DIRECT for non-graphical version
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -41,7 +46,7 @@ boxId = p.loadURDF("./simple.urdf", useFixedBase=True)
 
 numJoints = p.getNumJoints(boxId)
 for idx in range(numJoints):
-    print(f"{idx} {p.getJointInfo(boxId, idx)[1]}")
+    print(f"{idx} {p.getJointInfo(boxId, idx)[1]} {p.getJointInfo(boxId, idx)[12]}")
 
 # go to the starting position
 # p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, targetPosition=q0, controlMode=p.POSITION_CONTROL)
@@ -70,21 +75,39 @@ for t in logTime[1:]:
     #p.setJointMotorControl2(bodyIndex=boxId, jointIndex=1, force=u, controlMode=p.TORQUE_CONTROL)
     p.stepSimulation()
     jointState = p.getJointState(boxId, 1)
-    pos = jointState[0]
-    vel = jointState[1]
-    logPos[idx] = pos
-    logVel[idx-1] = vel
+    th1 = jointState[0]
+    dth1 = jointState[1]
+    logPos[idx] = th1
+    logVel[idx-1] = dth1
     idx += 1
 
-    xTheor = -np.sin(pos)*L
-    zTheor = Z0 - np.cos(pos)*L
+    xTheor1 = -np.sin(pos)*L
+    zTheor1 = Z0 - np.cos(pos)*L
+
+    th2 = p.getJointState(boxId, 3)[0]
+    dth2 = p.getJointState(boxId, 3)[1]
+    # xTheor2 = xTheor1 - L*np.sin(pos+pos2)
+    # zTheor2 = zTheor1 - L*np.cos(pos+pos2)
     
-    linkState = p.getLinkState(boxId, linkIndex=eefLinkIdx)
-    xSim = linkState[0][0]
-    zSim = linkState[0][2]
-    # print(f"THEOR: {xTheor} {zTheor}")
-    # print(f"SIMUL: {xSim} {zSim}")
-    # print(link_state[0])
+    # linkState = p.getLinkState(boxId, linkIndex=2)
+    # xSim1 = linkState[0][0]
+    # zSim1 = linkState[0][2]
+    # print(f"THEOR1: {xTheor1} {zTheor1}")
+    # print(f"SIMUL1: {xSim1} {zSim1}")
+
+    linkState = p.getLinkState(boxId, linkIndex=4)
+    xSim2 = linkState[0][0]
+    zSim2 = linkState[0][2]
+    # print(f"THEOR2: {xTheor2} {zTheor2}")
+    print(f"SIMUL2: {xSim2} {zSim2}")
+
+    J = np.array([[-L*np.cos(th1)-L*np.cos(th1+th2), -L*np.cos(th1+th2)], 
+                  [L*np.sin(th1)+L*np.sin(th1+th2), L*np.sin(th1+th2)]])
+    dth = np.array([[dth1],[dth2]])
+    
+    w = np.linalg.inv(J) @ -np.array([[xSim2-xd],[zSim2-zd]])
+    # print(w)
+    p.setJointMotorControlArray(bodyIndex=boxId, jointIndices=jointIndices, targetVelocities=[w[0,0],w[1,0]], controlMode=p.VELOCITY_CONTROL)
 
     time.sleep(dt)
     
